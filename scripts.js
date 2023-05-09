03811b16d3e4f808e0d295afe67d2f4b6c30b8d7
office <99219105+office@users.noreply.github.com>
2023-05-09 10:12:11 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,27 @@
-document.getElementById('go-to-task-list').addEventListener('click', () => {
-    window.location.href = 'task-list.html';
-});
-
-document.getElementById('go-to-dashboard').addEventListener('click', () => {
-    window.location.href = 'dashboard.html';
-});
+// Go to Task List button event listener
 if (document.getElementById('go-to-task-list')) {
     document.getElementById('go-to-task-list').addEventListener('click', () => {
         window.location.href = 'task-list.html';
     });
 }
 
+// Go to Dashboard button event listener
 if (document.getElementById('go-to-dashboard')) {
     document.getElementById('go-to-dashboard').addEventListener('click', () => {
+        window.location.href = 'dashboard.html';
+    });
+}
+
+// Go to Task List Creator button event listener
+if (document.getElementById('go-to-task-list-creator')) {
+    document.getElementById('go-to-task-list-creator').addEventListener('click', () => {
+        window.location.href = 'task-list.html';
+    });
+}
+
+// Go to User Dashboard button event listener
+if (document.getElementById('go-to-user-dashboard')) {
+    document.getElementById('go-to-user-dashboard').addEventListener('click', () => {
         window.location.href = 'dashboard.html';
     });
 }
