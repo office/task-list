c7c4931d4168c77332406772d1a6b9dd70573364
office <99219105+office@users.noreply.github.com>
2023-05-09 10:00:55 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('go-to-task-list').addEventListener('click', () => {
+    window.location.href = 'task-list.html';
+});
+
+document.getElementById('go-to-dashboard').addEventListener('click', () => {
+    window.location.href = 'dashboard.html';
+});
