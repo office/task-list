9edfefe769e745f07b2d06d8231cb5705ee8ebc4
office <99219105+office@users.noreply.github.com>
2023-05-09 10:08:37 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,3 +5,14 @@ document.getElementById('go-to-task-list').addEventListener('click', () => {
 document.getElementById('go-to-dashboard').addEventListener('click', () => {
     window.location.href = 'dashboard.html';
 });
+if (document.getElementById('go-to-task-list')) {
+    document.getElementById('go-to-task-list').addEventListener('click', () => {
+        window.location.href = 'task-list.html';
+    });
+}
+
+if (document.getElementById('go-to-dashboard')) {
+    document.getElementById('go-to-dashboard').addEventListener('click', () => {
+        window.location.href = 'dashboard.html';
+    });
+}
